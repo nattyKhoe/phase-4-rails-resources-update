0d4716844b7601931475cab2eb9d40bd79525a4c
Natasha <natasha.khoe@gmail.com>
2023-01-16 18:53:18 +0800

    done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -34,6 +34,17 @@ wrap_parameters format: []
     end
   end
 
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird, status: :ok
+    else
+      render json: {error: "Bird not found" }, status: :not_found
+    end
+  end
+  
+
   private
 
   def bird_params
